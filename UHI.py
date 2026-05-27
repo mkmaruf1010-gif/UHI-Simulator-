4d8a9e78aa78349da75cdb5c573f8cea69dd763c
Md Mehedi Khan Maruf <mkmaruf1616@gmail.com>
2026-05-27 18:31:17 +0600

    Update UHI.py

--- UHI.py
+++ UHI.py
@@ -110,7 +110,7 @@ with col1:
         f"is modeled to reduce the average surface temperature by **{abs(temperature_reduction):.2f}°C**."
     )
     st.caption("ℹ️ *The simulation renders a 100×100 grid overlaying micro-climate thermal zones onto your selected urban region. lat_span = 0.06 &
-lon_span = 0.06 ")
+lon_span = 0.06* ")
 
 with col2:
     # 8. Initialize Folium Map
