d3f30d3e75d20bdab9768823ac5242e997c8bdbc
Md Mehedi Khan Maruf <mkmaruf1616@gmail.com>
2026-09-06 13:05:48 +0600

    Update UHI.py

--- UHI.py
+++ UHI.py
@@ -10,11 +10,36 @@ st.set_page_config(page_title="Live UHI & Albedo Simulator", layout="wide")
 # Initialize Google Earth Engine securely
 @st.cache_resource
 def initialize_gee():
-    try:
-        ee.Initialize()
-    except Exception:
-        ee.Authenticate()
-        ee.Initialize()
+    credentials = ee.ServiceAccountCredentials(
+        st.secrets["gee"]["fieldmapping@fieldmapping-507507.iam.gserviceaccount.com"], 
+        key_data=st.secrets["gee"]["MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC+5su/HJ7aY4Kl
+JfxvzESry9SjsbYFp5q7JNgMbQaNQ9QxbQo68zq/63lkS9TVBE4Mzew4yp7OZSxf
+1sJZM+5VF+j1rqQbKA2s6rDQ0RzYtDdnfZDHx9qz5CZPGVbvQHNGj4YsZ48EtVRm
+Dn5z7PqDp+iefjg4W9veE2Qf8f9x2eCw+kYiYaIuXdAY+2oKPORobDtEnVlr5ZcP
+yzYVrEbxoVbcBKN8HmqjLCerYrQc57bMyVEfLbS7c9QXUBfor/oGSijRmrl1uZ6/
+hCqB5eXnZJLtDUMOs8EGn9xdVOUlz0q2pS4nIiPJpV8HH75NSSqZjGE/qmwq8OCH
+qHmzoIVvAgMBAAECggEAHhZP/GHtY2wVliPDgndGR3FLpTUIBCIjT+WBq4uvYapU
+EiomV54oH0Fkk3skIyKXjosR9O1yVAM8GCrH3LlDkeO/0qDgumr4pM0kavTaQAk/
+Ri9TFzmXbYzCwOq0eebFhFRwiCUWNF7D24XjRjAOV5uqHPi1saRApeKWgDZq1S2E
+2gyXpLYOv4RjvUumtx2ZU2Ki63Il+jJZzYMhmhy9ePICvI2OVohU5Mji/k+/YNkZ
+AGweEjq8k4SpLu47zUBurrb2HLXLoAiT0gDHC0mNb9N4i4koej2rr3/c3qkZSzhf
+XRbpj/yBBZSNzqjhofKxWTqxM3WP21fQZK4FudwWuQKBgQDrlc3fXxh7O0fgZiN3
+vIDl6mEGtjZcYzSYM1Oo2LzU92zXAqOhy9Npr541eI7fueQCKpPPZ8Q6DeK1RWli
+2pLDSgsmmn1Rpv6J5qvcBrwHQhgS5hpzuv6DHCLzfs9OO3Neowm7ErndTMFD6F0E
+vxRvQa6FamdhWRLQmSiTrhEQzQKBgQDPcbwo3kJAKoAoHbrTCAHs3vPTbGVqn3Kp
+jlj9TtJCX7xNUv1ZuKdQ1EgiJtzokkqq57oRNerVtJmN9tHlE1zWZN1P5fjwZk7s
+1JEyy4zKtaJYP2pFcMB4mgYImvW3U2eIG+DhOZUin1olM8LKvEXAYF3insqOelYg
+FDYhKSF/KwKBgBYW4Mvd/cyoPVAUI6U5fpiQRnK7qRM2lhrjTSfa0rHhVUo/zBoC
+o5sYRWxcFoFxy6hMV7mt0B9lZ+l5Ta1gMzyud6cx+ygp0Voaz+h8lSrVDq7O9pH9
+aZnfjINqU4PRXTr1bJcm5ViTttdoNTiZtg/Mh9GorXLaSEJY6g4W1zDNAoGBAIeZ
++T1dzEDVh2CxzmYUXe3Q7+HMgZ7pC7L6cgFjfN4WZqjFDwePRQ/5NA3fCZidyGFf
+iKmnbRK1M4sxRJNRzOX4mRKZadj7h6wYZ7GkjSwU/0Jgcb9irO4pALivUt+7jXnT
+f3S6h1pSgELBSxvrZk0SsUxqME5iedoOXDPhBxxxAoGBAKwDfacgj/nblvXspTyD
+nnOVneITTLkfJgOTgLT0b/xECi7BJW+0egvJjtcJWRniJn2RRswmwy/OzSERmvs6
+S3vCfn7Lt59In/X7fx3KK+3vS/sonOwE7VF9qPnom0ico/wSKsuPh5w4ghS6toae
++9IQEJIF6qqsvKJNqQxGSdvK"]
+    )
+    ee.Initialize(credentials)
 
 initialize_gee()
 
